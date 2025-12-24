c2afa9ae7520392dd3b4c2c31680499960bd2981
GreyHak <479851+GreyHak@users.noreply.github.com>
2025-12-24 09:04:38 -0800

    sav_to_html: Update dimensional depot parsing to align with save format

--- sav_to_html.py
+++ sav_to_html.py
@@ -291,7 +291,7 @@ def generateHTML(savFilename: str, outputDir: str = DEFAULT_OUTPUT_DIR, htmlBase
                   for storedItem in storedItems:
                      itemClass = sav_parse.getPropertyValue(storedItem[0], "ItemClass")
                      if itemClass is not None:
-                        amount = sav_parse.getPropertyValue(storedItem[0], "amount")
+                        amount = sav_parse.getPropertyValue(storedItem[0], "Amount")
                         if amount is not None:
                            itemName = sav_parse.pathNameToReadableName(itemClass.pathName)
                            dimensionalDepotContents.append((amount, itemName))
